ed7f6074ce42a47a190e7a5ea0a6e8594c778e5f
Sebastian Matuszczyk <s.matuszczyk1@gmail.com>
2022-04-22 01:14:27 +0200

    Winning system
    winning screen
    score system

--- Assets/Scripts/Objects/Collector.cs
+++ Assets/Scripts/Objects/Collector.cs
@@ -1,17 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Collector : MonoBehaviour
 {
     [SerializeField] float destroyDelay = 0f;
     bool hasCube;
+    public GameObject scoreText;
+    public static int theScore;
+
 
     void Start()
     {
         
     }
 
+    void Update(){
+        scoreText.GetComponent<Text>().text = "Score: " + theScore;
+    }
     public void OnTriggerEnter(Collider other) {
         if(other.tag == "Cube" && !hasCube){
             Debug.Log("Pick");
@@ -22,6 +30,12 @@ public class Collector : MonoBehaviour
         if(other.tag == "DeliveryZone" && hasCube){
             Debug.Log("Delivered");
             hasCube = false;
+            theScore += 1;
+        }
+
+        if(theScore == 4){
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+            theScore = 0;
         }
     }
 }
